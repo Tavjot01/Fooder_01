2797e3a2a7f854a2d4dd33b9250d814547580b8b
Tavjot <112978666+Tavjot01@users.noreply.github.com>
2023-09-07 19:52:28 +0200

    New Update commit

--- app/src/main/java/com/example/fooder/MainActivity.kt
+++ app/src/main/java/com/example/fooder/MainActivity.kt
@@ -6,7 +6,10 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.view.animation.AccelerateInterpolator
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -16,5 +19,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout)
+
+
+        val password = findViewById<EditText>(R.id.password)
+        val username = findViewById<EditText>(R.id.username)
+        val button =  findViewById<Button>(R.id.Submit)
+
+        button.setOnClickListener(){
+            val username_Input =  username.text.toString()
+            val password = password.text.toString()
+
+            if(username.equals("Ekam") && password == "1234"){
+                print(" Welcome to Fooder! ")
+
+            }
+        }
 
     }}
