08505ba95aa5e01d83effb960b7e6cb4d6c06d5b
lazyworkbug <thisisgood0011@gmail.com>
2023-09-07 22:38:38 +0530

    nice

--- app/src/main/java/com/example/fooder/MainActivity.kt
+++ app/src/main/java/com/example/fooder/MainActivity.kt
@@ -1,28 +1,20 @@
 package com.example.fooder
 
-import android.os.Bundle
+
 import android.animation.ObjectAnimator
-import android.view.animation.AccelerateInterpolator;
-import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.widget.TextView
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.layout)
 
-        val animatedText: TextView = findViewById(R.id.apploadpage)
-
-        // Create an ObjectAnimator to animate the alpha property
-        val fadeInAnimator = ObjectAnimator.ofFloat(animatedText, "text", 0f, 1f)
-        fadeInAnimator.duration = 1000 // Duration in milliseconds
-        fadeInAnimator.interpolator = AccelerateInterpolator()
-
-        // Start the animation
-        fadeInAnimator.start()
-    }
-
-}
+    }}
