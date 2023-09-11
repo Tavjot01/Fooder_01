e09b524d547b93a84e9732ac4ea9160d147a4de5
Tavjot <112978666+Tavjot01@users.noreply.github.com>
2023-09-11 20:33:18 +0200

    new

---
+++ app/src/main/java/com/example/fooder/Restaurant_HomePage.kt
@@ -0,0 +1,12 @@
+package com.example.fooder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class Restaurant_HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurant_home_page)
+    }
+}
